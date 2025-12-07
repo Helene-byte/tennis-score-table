74bb8f5c98ee21c7b7fc81421ac00334c115db27
Olena_Krasylshchykova <olena_krasylshchykova@epam.com>
2025-12-07 20:16:11 +0100

    fix: add web.xml + score classes and test

---
+++ IdeaProjects/tennis-score-table/src/main/java/org/example/service/OngoingMatchesService.java
@@ -0,0 +1,29 @@
+package org.example.service;
+
+import org.example.model.entity.Match;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class OngoingMatchesService {
+    private final Map<UUID, Match> ongoingMatches = new ConcurrentHashMap<>();
+
+    public UUID createMatch(Match match) {
+        UUID matchId = UUID.randomUUID();
+        ongoingMatches.put(matchId, match);
+        return matchId;
+    }
+
+    public Match getMatch(UUID matchId) {
+        return ongoingMatches.get(matchId);
+    }
+
+    public void removeMatch(UUID matchId) {
+        ongoingMatches.remove(matchId);
+    }
+
+    public boolean containsMatch(UUID matchId) {
+        return ongoingMatches.containsKey(matchId);
+    }
+}
